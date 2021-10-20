a4b5e51010c2d049bb17ed37772f4048f2d53771
Allen Tran <allentran08@gmail.com>
2021-10-20 15:40:16 -0700

    migrating pages over

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("nav-bar").style.display = "block";
+  document.getElementById("menu-button").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("nav-bar").style.display = "none";
+  document.getElementById("menu-button").style.display = "block";
+}
